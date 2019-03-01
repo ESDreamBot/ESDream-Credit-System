e18d2ee772c1f929cc28e55ee54bb18769c68934
ESDreamBot <47387876+ESDreamBot@users.noreply.github.com>
2019-03-01 07:35:42 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -244,7 +244,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : ESDream ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`=credit`,"WATCHING")
+client.user.setGame(`=credit | =daily`,"PLAYING")
 client.user.setStatus("online")
 });
 
